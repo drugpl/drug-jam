c98ba9a81a5a7103da5b0d5145b30bea3015a6fa
Paweł Pacana <pawel.pacana@gmail.com>
2011-01-06 16:41:08 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'pg'
+gem 'haml'
+gem 'haml-rails'
+gem 'devise'
+gem 'simple_form'
+gem 'RedCloth'
+gem 'automatic_foreign_key'
+gem 'jquery-rails', '>= 0.2.6'
+gem 'rspec-rails', :group => [:test, :development]
+
+group :test do
+  gem 'rspec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jam::Application.config.secret_token = '23b32176cc395811db8c45342ac62c32a7cc96441d642255dcee440d5fe4bfa79d1a838ccea60ab17d3057270aaa45cadc990c4a17d913978dbdc8e50cd0e6c6'
