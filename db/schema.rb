7a3a733bd11b32ee3b86d58856a3eaf069e1d511
Paweł Pacana <pawel.pacana@gmail.com>
2011-01-25 00:38:54 +0100

    session store in db and removed token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110109125643) do
+ActiveRecord::Schema.define(:version => 20110124233722) do
 
   create_table "attendances", :force => true do |t|
     t.integer  "attendant_id", :null => false
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(:version => 20110109125643) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "submissions", :force => true do |t|
     t.integer  "author_id",   :null => false
